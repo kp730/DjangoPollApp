2dc3b37c4ac0baf2c8c5c16554f16b91f1b17e72
kp730 <krish170123028@iitg.ac.in>
2019-12-18 19:08:09 +0530

    Added tests and resolved some errors using them

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect
 from .models import Question, Choice
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 class IndexView(generic.ListView):
     template_name='polls/index.html'
@@ -10,11 +11,13 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last 5 published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
     template_name='polls/detail.html'
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
